c27fb9f817f6bb5b0972bf705ef714b446665a31
rafcollier <rogercollier@hotmail.com>
2019-09-29 16:14:24 -0400

    heroku database link added

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/nbaStats',
-  //database: 'mongodb://heroku_k52f5vsp:efhqgaaq5augddhfj8g61hb7l4@ds123490.mlab.com:23490/heroku_k52f5vsp',
+  //database: 'mongodb://localhost:27017/nbaStats',
+  database: 'mongodb://heatcheck:heatcheck1@ds147684.mlab.com:47684/heroku_1nvnbvnq',
   secret: 'nbaStatsSecret'
 }
